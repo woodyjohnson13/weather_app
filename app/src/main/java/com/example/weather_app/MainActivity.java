94387fa91675f630584b9c94f93a8d024bbbf25e
woodyjohnson13 <johnsonwoody13@gmail.com>
2022-05-26 11:58:08 +0300

    First interface steps done.

--- app/src/main/java/com/example/weather_app/MainActivity.java
+++ app/src/main/java/com/example/weather_app/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.weather_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText city_name,country_name;
+    Button get_info;
+    TextView result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void getWeatherDetails(View view) {
     }
 }
